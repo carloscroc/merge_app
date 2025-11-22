b6e3643e319450abfbb630121d0f3b18a20f166f
carloscroc <carloscroc@yahoo.com>
2025-11-22 18:48:32 -0500

    fix: use super.key in HomePage constructor

--- lib/src/pages_cupertino/home_page.dart
+++ lib/src/pages_cupertino/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   static const String routeName = 'HomePage';
   static const String routePath = '/homePage';
